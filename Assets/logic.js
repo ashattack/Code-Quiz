fd8aa33151d4454abd1279461fc19551cc31852d
Ashlyn Hicks <ash.giselle96@gmail.com>
2020-12-22 19:36:16 -0500

    added highscores input

--- Assets/logic.js
+++ Assets/logic.js
@@ -3,6 +3,7 @@ var startBtn = $("#start");
 var secondsLeft = 60
 var secondsText = $('#secondsLeft')
 var onQuestion = 0;
+var quizTime;
 
 
 var endGame = $("#end-game")
@@ -13,61 +14,81 @@ questionsElement.hide()
 
 // Function for kicking off quiz
 function startQuiz() {
-    console.log("start quiz");
-    var startScreen = $("#start-screen");
-    startScreen.hide()
+  console.log("start quiz");
+  var startScreen = $("#start-screen");
+  startScreen.hide()
 
-    //Unhide the questions 
-    questionsElement.removeAttr("style")
-    writeNextQuestion();
-    setTime()
+  //Unhide the questions 
+  questionsElement.removeAttr("style")
+  writeNextQuestion();
+  $(".choice").on("click", onQuestionClick);
+  setTime()
 }
 
 startBtn.on('click', startQuiz)
 
 //Setting the timer
 function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      console.log(secondsLeft)
-      $('#secondsLeft').html(secondsLeft)
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-      }
-  
-    }, 1000);
-  }
- //Adding text to the buttons
-  function writeQuestion(questions) {
-    $("#card-title").html(questions.title);
-    $("#button1").html(questions.choices[0]);
-    $("#button2").html(questions.choices[1]);
-    $("#button3").html(questions.choices[2]);
-    $("#button4").html(questions.choices[3]);
-    $(".choice").on("click", onQuestionClick);
-  }
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    console.log(secondsLeft)
+    $('#secondsLeft').html(secondsLeft)
 
-    //What happens when someone clicks an answer
-  function onQuestionClick(e) {
-    console.log(e);
-    var target = $(this);
-    var answerClick = target.html();
-    console.log(answerClick);
-    var answer = questions[onQuestion].answer;
-    if (answer != answerClick){
-        secondsLeft =- 10;
+    if (secondsLeft === 0) {
+
+      clearInterval(timerInterval);
+      endQuiz();
+
     }
-    writeNextQuestion();
+
+  }, 1000);
+}
+//Adding text to the buttons
+function writeQuestion(questions) {
+  $("#card-title").html(questions.title);
+  $("#button1").html(questions.choices[0]);
+  $("#button2").html(questions.choices[1]);
+  $("#button3").html(questions.choices[2]);
+  $("#button4").html(questions.choices[3]);
+}
+
+//What happens when someone clicks an answer
+function onQuestionClick(e) {
+  console.log(e);
+  var target = $(this);
+  var answerClick = target.html();
+  console.log(answerClick);
+  var answer = questions[onQuestion].answer;
+  if (answer != answerClick) {
+    secondsLeft -= 5;
   }
+  writeNextQuestion();
+}
 //moves onto the next question
-  function writeNextQuestion() {
-      var question = questions[onQuestion]
-      writeQuestion(question)
-      onQuestion++
+function writeNextQuestion() {
+  var question = questions[onQuestion]
+  writeQuestion(question)
+  onQuestion++
+  if (onQuestion >= questions.length) {
+    endQuiz();
   }
+}
 
-  function endQuiz() {
+function endQuiz() {
+  quizTime = secondsLeft;
+  questionsElement.hide()
+  endGame.removeAttr("style")
+  $("#secondsLeft").attr("id", "quiz-time")
+}
 
-  }
+// what happens when you click view highscores
+function highScoresBtn(e) {
+  var value = $("#initials-here").val();
+  var highScores = localStorage.getItem("high");
+  localStorage.setItem("high", highscores + "," + value + ":" + quizTime);
+
+
+
+}
+$("#highscores").on("click", highScoresBtn);
 
--- Assets/questions.js
+++ Assets/questions.js
@@ -1,32 +1,32 @@
 var questions = [
-    {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-      },
-      {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-      },
-      {
-        title: "Is JavaScript fun to work with?",
-        choices: ["No", "Sometimes", "What is Javascript", "Not just yes, but HELL YES!"],
-        answer: "Not just yes, but HELL YES!"
-      },
-      {
-        title: "DOM is an abreviation for ____",
-        choices: ["Data Object Mode", "Dumb Old Man", "Document Object Model", "Dutle Opo Mipsy"],
-        answer: "Document Object Model"
-      },
-      {
-        title: "JavaScript is Textile Mark Up (TML) version of Java?",
-        choices: ["True", "False"],
-        answer: "False"
-      },
-      {
-        title: "JavaScript is strongly typed language",
-        choices: ["True", "False"],
-        answer: "False"
-      }
+  {
+    title: "Iroh is a member of which secret socierty?",
+    choices: ["Order of the White Lotus", "Travelers of the Nation", "Order of the Whisper Lotus", "Ginseng Tea Masters"],
+    answer: "Order of the White Lotus"
+  },
+  {
+    title: "How did Zuko get a scar on his face?",
+    choices: ["Firebending with friends", "By being mischevious as a kid", "A demonstration gone wrong at school", "His father Ozai gave it to him in a duel"],
+    answer: "His father Ozai gave it to him in a duel"
+  },
+  {
+    title: "What two words make Appa fly?",
+    choices: ["Up-Up", "Come on", "Pip-pip", "Yip-yip"],
+    answer: "Yip-yip"
+  },
+  {
+    title: "What kind of bending does Katara swear to never do again?",
+    choices: ["Metalbending", "Bloodbending", "Tearbending", "Swampbending"],
+    answer: "Bloodbending"
+  },
+  {
+    title: "What weapon does Sokka carry?",
+    choices: ["Bow and Arrow", "Knives", "Boomerang", "He doesn't, he's a water bender"],
+    answer: "Boomerang"
+  },
+  {
+    title: "Who does Aang have a big crush on?",
+    choices: ["Katara", "Azula", "Toph", "None of the above"],
+    answer: "Katara"
+  }
 ]
